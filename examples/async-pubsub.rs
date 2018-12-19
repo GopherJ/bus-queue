aa67fddfb16f1685c074b150f1340a4c9bdc3595
Vladan Popovic <vladanovic@gmail.com>
2018-12-19 18:12:08 +0100

    Redo async example

--- examples/async-pubsub.rs
+++ examples/async-pubsub.rs
@@ -2,39 +2,40 @@ extern crate bus_queue;
 extern crate futures;
 extern crate tokio;
 
-use bus_queue::async::AsyncBus;
-use std::thread;
+use bus_queue::async::*;
+use std::thread::*;
 use std::time;
-use thread::current;
 use tokio::prelude::*;
 
 fn main() {
     let mut async_bus: AsyncBus<u32> = AsyncBus::new(10);
 
-    let mut streams = Vec::new();
-    for _i in 0..10 {
-        streams.push(async_bus.add_sub());
+    fn produce(async_bus: &mut AsyncBus<u32>) -> () {
+        println!("enter producer!");
+        sleep(time::Duration::from_millis(2000));
+        for i in 0..40 {
+            async_bus.push(i);
+            sleep(time::Duration::from_millis(500));
+        }
     }
-    let mut vec = Vec::new();
-    for stream in streams {
-        vec.push(thread::spawn(move || {
+
+    fn consume(consumers: Vec<AsyncBusReader<u32>>) -> () {
+        println!("enter consumers {}!", consumers.len());
+        for stream in consumers {
             let future = stream.for_each(|curr| {
                 println!("fut {:?} : {}", current().id(), curr);
                 futures::future::ok(())
             });
             tokio::run(future);
-        }));
-    }
-    let a = thread::spawn(move || {
-        thread::sleep(time::Duration::from_millis(2000));
-        for i in 0..40 {
-            async_bus.push(i);
-            thread::sleep(time::Duration::from_millis(500));
         }
-    });
+        ()
+    }
+
+    let streams = (0..10).map(|_| async_bus.add_sub()).collect();
+
+    let a = spawn(move || consume(streams));
+    let b = spawn(move || produce(&mut async_bus));
 
     a.join().unwrap();
-    for th in vec {
-        th.join().unwrap();
-    }
+    b.join().unwrap();
 }
